ba925ca0ab40d6d49ada5ee9718e24ebff620fd9
Taizo_Ayase <zxcvbnmkind@gmail.com>
2013-04-22 01:44:59 +0900

    Write keyword in config file

--- main.rb
+++ main.rb
@@ -3,10 +3,13 @@
 require "ap"
 require "./publication"
 require "./article"
+require "yaml"
+
+config = YAML.load("conf.yaml")
 
 #f = "./labwiki_before.txt"
 out = Publication.new
-pub = PubMedArticle.search("Nureki O")
+pub = PubMedArticle.search(config["keyword"])
 pub.each do |id|
 	out << PubMedArticle.new(id)
 end
